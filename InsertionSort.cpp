76c9c1c7be5354973d4ea9f539edb8bd9c5ce968
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-03-26 21:45:41 +0700

    membuat jarak dan tampilan data serta melakukan perulangan

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -20,4 +20,14 @@ void input ()
             cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
         }
     }
+    cout << endl; // Membuat jarak per baris program
+    cout << "=======================" << endl; // Membuat tampilan susunan data element Array
+    cout << "Masukkan Elment Array" << endl;
+    cout << "=======================" << endl;
+
+    for (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
+    {
+        cout << "Data ke-" << (i + 1) << ": "; // memasukkan atau menginputkan nilai data n
+        cin >> arr[i]; // Menyimpan nilai data n kedalam array arr
+    }
 }
