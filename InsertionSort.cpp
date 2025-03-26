a5f94f4ca353d5de8c1c942635e988ed56116ba6
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-03-26 21:39:27 +0700

    Membuat dan Memanggil nputan n pada Array, serta membuat kondisi n tidak lebih dari 20

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -3,3 +3,21 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengn panjang data 20
 int n; // Membuat Variable inputan n
+
+void input ()
+{ // Procedure Input
+    while (true)
+    {
+        cout << "masukkan Jumlah Data pada Array : "; // membuat Inputan jumlah element array
+        cin >> n; // memanggil variable inputan n
+
+        if (n <= 20)
+        { // Membuat Kondisi n tidak lebih dari 20
+            break;
+        }
+        else
+        {
+            cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+    }
+}
